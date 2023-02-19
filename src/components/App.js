7a2780845bf92234f70343f68c1ba35e61bad285
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-19 14:09:20 +0530

    contest

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
   const handleClick = (event) =>{
     // use console.log
-    console.log(`Button id is:-${event.tagret.id}`);
+    console.log(`Button id is:-${event.target.id}`);
 
   }
 
